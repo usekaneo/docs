242f6162f7ef21d708ca7e9ebeef67cca63b4097
Andrej Acevski <andrejacevski@pantone.com>
2024-12-23 22:39:26 +0100

    add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: netlify(),
 });
