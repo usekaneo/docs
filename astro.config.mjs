f4639dcdea54f7137aaaa1bc1b187f9c2585b0c1
Andrej <44305048+aacevski@users.noreply.github.com>
2025-01-04 19:26:27 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://kaneo-app.github.io',
-  base: '/landing-page',
+  site: 'https://kaneo.app',
+  base: '/',
 });
