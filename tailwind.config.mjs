52027d88ee80f38488ca4337377a0523d100eafd
Andrej <aacevski@gmail.com>
2025-02-14 00:48:42 +0100

    feat: adding dark / light mode on landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -19,6 +19,7 @@ const gray = {
 
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: ["class"],
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
